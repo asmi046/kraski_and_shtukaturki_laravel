f378dcc04e23666fc0ea8e20058e329db669d0eb
asmi046 <asmi046@gmail.com>
2023-09-07 21:16:43 +0300

    first commit

---
+++ resources/views/layouts/all.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <title>@yield('title') - Краски и Штукатурки</title>
+    <meta name="description" content="@yield('description')">
+
+    <meta property="og:locale" content="ru_RU" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="@yield('title') />
+    <meta property="og:description" content="@yield('description')" />
+    <meta property="og:url" content="{{route('home')}}" />
+    <meta property="og:site_name" content="Магазин индийских товаров - Mini India" />
+    <meta property="og:image" content="{{asset('img/og_img.jpg')}}" />
+    <meta property="og:image:type" content="image/jpeg" />
+    <meta name="twitter:card" content="summary_large_image" />
+
+	<meta charset="UTF-8">
+	<meta name="format-detection" content="telephone=no">
+
+    <link rel="icon" type="image/png" href="{{asset('/img/favicons/icon256.png')}}" sizes="256x256">
+    <link rel="icon" type="image/png" href="{{asset('/img/favicons/icon128.png')}}" sizes="128x128">
+    <link rel="icon" type="image/png" href="{{asset('/img/favicons/icon64.png')}}" sizes="64x64">
+    <link rel="icon" type="image/png" href="{{asset('/img/favicons/icon32.png')}}" sizes="32x32">
+    <link rel="icon" type="image/png" href="{{asset('/img/favicons/icon16.png')}}" sizes="16x16">
+    <link rel="icon" type="image/svg" href="{{asset('/img/favicons/logo-mini.svg')}}" sizes="any">
+
+    <meta name="_token" content="{{ csrf_token() }}">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    @vite([
+        'resources/css/app.css',
+        'resources/js/app.js',
+
+        'public/css/main.css',
+        'public/css/header.css',
+        'public/css/banner.css',
+        'public/css/product-card.css',
+        'public/css/advantages.css',
+        'public/css/category.css',
+        'public/css/cta-blk.css',
+        'public/css/footer.css',
+    ])
+
+</head>
+
+<body>
+	<div class="wrapper" id="global_app">
+        <x-header></x-header>
+
+        @yield('content')
+
+        <x-footer></x-footer>
+    </div>
+</body>
+
+</html>
