fa157fdcc38bdabdcb5461091466fc7d96b7de1d
Ruvick <rudikov.web@ya.ru>
2023-09-16 23:00:13 +0300

    Новая секция Популярные

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -104,6 +104,89 @@
 	</div>
 </section>
 
+<section class="page__popular popular section">
+	<div class="_container">
+		<h2 class="popular__title title">Популярные товары</h2>
+
+		<div class="popular__inner d-flex">
+
+			<div class="popular__column">
+				<div class="popular__card card-popular">
+					<div class="card-popular__img _ibg">
+						<img src="@img" alt="">
+					</div>
+					<div class="card-popular__descp">
+						<h3 class="card-popular__descp-title">Maggiore</h3>
+						<p class="card-popular__descp-subtitle">
+							Суперстойкая глубоко — матовая 
+							краска для создания идеальных 
+							поверхностей
+						</p>
+						<div class="card-popular__descp-price">от 1 760 руб.</div>
+					</div>
+					<a href="#" class="card-popular__btn">Подробнее...</a>
+				</div>
+			</div>
+
+			<div class="popular__column">
+				<div class="popular__card card-popular">
+					<div class="card-popular__img _ibg">
+						<img src="@img" alt="">
+					</div>
+					<div class="card-popular__descp">
+						<h3 class="card-popular__descp-title">Maggiore</h3>
+						<p class="card-popular__descp-subtitle">
+							Суперстойкая глубоко — матовая 
+							краска для создания идеальных 
+							поверхностей
+						</p>
+						<div class="card-popular__descp-price">от 1 760 руб.</div>
+					</div>
+					<a href="#" class="card-popular__btn">Подробнее...</a>
+				</div>
+			</div>
+
+			<div class="popular__column">
+				<div class="popular__card card-popular">
+					<div class="card-popular__img _ibg">
+						<img src="@img" alt="">
+					</div>
+					<div class="card-popular__descp">
+						<h3 class="card-popular__descp-title">Maggiore</h3>
+						<p class="card-popular__descp-subtitle">
+							Суперстойкая глубоко — матовая 
+							краска для создания идеальных 
+							поверхностей
+						</p>
+						<div class="card-popular__descp-price">от 1 760 руб.</div>
+					</div>
+					<a href="#" class="card-popular__btn">Подробнее...</a>
+				</div>
+			</div>
+
+			<div class="popular__column">
+				<div class="popular__card card-popular">
+					<div class="card-popular__img _ibg">
+						<img src="@img" alt="">
+					</div>
+					<div class="card-popular__descp">
+						<h3 class="card-popular__descp-title">Maggiore</h3>
+						<p class="card-popular__descp-subtitle">
+							Суперстойкая глубоко — матовая 
+							краска для создания идеальных 
+							поверхностей
+						</p>
+						<div class="card-popular__descp-price">от 1 760 руб.</div>
+					</div>
+					<a href="#" class="card-popular__btn">Подробнее...</a>
+				</div>
+			</div>
+
+		</div>
+
+	</div>
+</section>
+
 <section class="page__about about section">
 	<div class="_container">
 		<h2 class="about__title title">О нас</h2>
