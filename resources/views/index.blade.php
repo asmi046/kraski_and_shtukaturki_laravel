8b34b029834cbfe5cc31aadc2cd21de7eb671b92
Ruvick <rudikov.web@ya.ru>
2023-09-16 21:27:46 +0300

    Популярные товары

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,12 +21,99 @@
 	</div>
 </section>
 
+<section class="page__popular popular section">
+	<div class="_container">
+		<h2 class="popular__title title">Популярные товары</h2>
+
+		<div class="popular__inner d-flex">
+
+			<div class="popular__column">
+				<div class="popular__card card-popular">
+					<div class="card-popular__img _ibg">
+						<img src="@img" alt="">
+					</div>
+					<div class="card-popular__descp">
+						<h3 class="card-popular__descp-title">Maggiore</h3>
+						<p class="card-popular__descp-subtitle">
+							Суперстойкая глубоко — матовая 
+							краска для создания идеальных 
+							поверхностей
+						</p>
+						<div class="card-popular__descp-price">от 1 760 руб.</div>
+					</div>
+					<a href="#" class="card-popular__btn">Подробнее...</a>
+				</div>
+			</div>
+
+			<div class="popular__column">
+				<div class="popular__card card-popular">
+					<div class="card-popular__img _ibg">
+						<img src="@img" alt="">
+					</div>
+					<div class="card-popular__descp">
+						<h3 class="card-popular__descp-title">Maggiore</h3>
+						<p class="card-popular__descp-subtitle">
+							Суперстойкая глубоко — матовая 
+							краска для создания идеальных 
+							поверхностей
+						</p>
+						<div class="card-popular__descp-price">от 1 760 руб.</div>
+					</div>
+					<a href="#" class="card-popular__btn">Подробнее...</a>
+				</div>
+			</div>
+
+			<div class="popular__column">
+				<div class="popular__card card-popular">
+					<div class="card-popular__img _ibg">
+						<img src="@img" alt="">
+					</div>
+					<div class="card-popular__descp">
+						<h3 class="card-popular__descp-title">Maggiore</h3>
+						<p class="card-popular__descp-subtitle">
+							Суперстойкая глубоко — матовая 
+							краска для создания идеальных 
+							поверхностей
+						</p>
+						<div class="card-popular__descp-price">от 1 760 руб.</div>
+					</div>
+					<a href="#" class="card-popular__btn">Подробнее...</a>
+				</div>
+			</div>
+
+			<div class="popular__column">
+				<div class="popular__card card-popular">
+					<div class="card-popular__img _ibg">
+						<img src="@img" alt="">
+					</div>
+					<div class="card-popular__descp">
+						<h3 class="card-popular__descp-title">Maggiore</h3>
+						<p class="card-popular__descp-subtitle">
+							Суперстойкая глубоко — матовая 
+							краска для создания идеальных 
+							поверхностей
+						</p>
+						<div class="card-popular__descp-price">от 1 760 руб.</div>
+					</div>
+					<a href="#" class="card-popular__btn">Подробнее...</a>
+				</div>
+			</div>
+
+		</div>
+
+	</div>
+</section>
+
 <section class="page__about about section">
 	<div class="_container">
-
-		<h2 class="about__title title">Загловок</h2>
-		<p class="about__subtitle">Подзагловок</p>
-
+		<h2 class="about__title title">О нас</h2>
+		<div class="about__subtitle">
+			<p>
+			Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" 
+			для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя 
+			Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн.
+			</p>
+		</div>
 	</div>
 </section>
 
