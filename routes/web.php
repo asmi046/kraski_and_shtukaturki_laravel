935aa728d7ac8605b51d0f62d15c3d8c9804d183
asmi046 <asmi046@gmail.com>
2023-09-27 21:08:50 +0300

    Роуты

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,11 @@ use App\Http\Controllers\IndexController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 
+use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\CartController;
+
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,3 +24,20 @@ use App\Http\Controllers\ProductController;
 Route::get('/', [IndexController::class, "show"])->name('home');
 Route::get('/catalog', [CategoryController::class, "show"])->name('catalog');
 Route::get('/product', [ProductController::class, "show"])->name('product');
+
+Route::get('/bascet/thencs', [CartController::class, "thencs"])->name("bascet_thencs");
+Route::get('/bascet', [CartController::class, "index"])->name("bascet");
+Route::post('/bascet/add', [CartController::class, "add"])->name("bascet_add");
+Route::post('/bascet/update', [CartController::class, "update"])->name("bascet_update");
+Route::get('/bascet/get', [CartController::class, "get_all"])->name("bascet_get");
+Route::delete('/bascet/clear', [CartController::class, "clear"])->name("bascet_clear");
+Route::delete('/bascet/delete', [CartController::class, "delete"])->name("bascet_delete");
+Route::post('/bascet/send', [CartController::class, "send"])->name("bascet_send");
+Route::post('/bascet/ocsend', [CartController::class, "send_oc"])->name("bascet_oc_send");
+
+Route::get('/favorites', [FavoriteController::class, "index"])->name("favorites");
+Route::get('/favorites/get', [FavoriteController::class, "get_all"])->name("favorites_get");
+Route::post('/favorites/add', [FavoriteController::class, "add"])->name("favorites_add");
+Route::delete('/favorites/delete', [FavoriteController::class, "delete"])->name("favorites_delete");
+Route::delete('/favorites/clear', [FavoriteController::class, "clear"])->name("favorites_clear");
+
