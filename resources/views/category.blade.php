054470e8a29e20066e41707a962d813c404c92b1
Ruvick <rudikov.web@ya.ru>
2023-09-28 13:20:22 +0300

    Стр Категории c адаптовом

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -8,7 +8,249 @@
 @section('title', $title)
 @section('description', $description)
 
-@section('content')
+@section('content')  
 
+<section class="banner-category banner">
+	<div class="nuar_blk"></div>
+	<div class="_container">
 
-@endsection
+		<h1 class="banner-category__title">
+			ИНТЕРЬЕРНЫЕ КРАСКИ
+		</h1>
+
+	</div>
+</section>
+
+<section class="categoty-top-sec section">
+	<div class="_container">
+
+	<div class="categoty-top-sec__breadcrumbs">
+	<p id="breadcrumbs">
+	<span>
+		<span>
+			<a href="index.html">
+				Главная
+			</a> /
+			<a href="index.html">
+				Структурная штукатурка
+			</a> /
+			<span class="breadcrumb_last" aria-current="page">
+				Stucco in Rilievo
+			</span>
+		</span>
+	</span>
+</p>
+	</div>
+
+<div class="categoty-top-sec__filter-block filter-block">
+	<button class="filter-block__btn btn _active">Каталог товаров</button>
+	<button class="filter-block__btn btn">Декоративные краски</button>
+	<button class="filter-block__btn btn">Венецианская штукатурка</button>
+	<button class="filter-block__btn btn">Фактурные покрытия</button>
+	<button class="filter-block__btn btn">Финишные покрытия</button>
+	<button class="filter-block__btn btn">Микроцемент</button>
+</div>
+
+	</div>
+</section>
+
+<section class="category-sec section">
+	<div class="_container">
+
+<div class="category-sec__wrap">
+
+	<div class="category-sec__sidebar sidebar">
+		<h1 class="sidebar__title">Название категории</h1>
+
+		<form action="#" class="sidebar__filters filters-sidebar">
+
+			<div class="filters-sidebar__block">
+
+			</div>
+
+			<div class="filters-sidebar__block">
+				
+			</div>
+
+			<button type="reset" class="filters-sidebar__btn-reset btn">Сбросить фильтры</button>
+
+		</form>
+
+	</div>
+
+	<div class="category-sec__main main-category">
+
+	<div class="main-category__inner">
+
+	<div class="popular__column">
+				<div class="popular__card card-popular">
+					<div class="card-popular__img _ibg">
+						<img src="img/popular/01.jpg" alt="Картинка товара">
+					</div>
+					<div class="card-popular__descp">
+						<h3 class="card-popular__descp-title">Maggiore</h3>
+						<p class="card-popular__descp-subtitle">
+							Суперстойкая глубоко — матовая
+							краска для создания идеальных
+							поверхностей
+						</p>
+						<div class="card-popular__descp-price">от 1 760 руб.</div>
+					</div>
+					<a href="#" class="card-popular__btn">Подробнее...</a>
+				</div>
+			</div>
+
+			<div class="popular__column">
+				<div class="popular__card card-popular">
+					<div class="card-popular__img _ibg">
+					<img src="img/popular/01.jpg" alt="Картинка товара">
+					</div>
+					<div class="card-popular__descp">
+						<h3 class="card-popular__descp-title">Rotonda</h3>
+						<p class="card-popular__descp-subtitle">
+							Суперстойкая глубоко — матовая
+							краска для создания идеальных
+							поверхностей
+						</p>
+						<div class="card-popular__descp-price">от 1 760 руб.</div>
+					</div>
+					<a href="#" class="card-popular__btn">Подробнее...</a>
+				</div>
+			</div>
+
+			<div class="popular__column">
+				<div class="popular__card card-popular">
+					<div class="card-popular__img _ibg">
+					<img src="img/popular/02.jpg" alt="Картинка товара">
+					</div>
+					<div class="card-popular__descp">
+						<h3 class="card-popular__descp-title">Maggiore</h3>
+						<p class="card-popular__descp-subtitle">
+							Суперстойкая глубоко — матовая
+							краска для создания идеальных
+							поверхностей
+						</p>
+						<div class="card-popular__descp-price">от 1 760 руб.</div>
+					</div>
+					<a href="#" class="card-popular__btn">Подробнее...</a>
+				</div>
+			</div>
+
+			<div class="popular__column">
+				<div class="popular__card card-popular">
+					<div class="card-popular__img _ibg">
+					<img src="img/popular/01.jpg" alt="Картинка товара">
+					</div>
+					<div class="card-popular__descp">
+						<h3 class="card-popular__descp-title">Rotonda</h3>
+						<p class="card-popular__descp-subtitle">
+							Суперстойкая глубоко — матовая
+							краска для создания идеальных
+							поверхностей
+						</p>
+						<div class="card-popular__descp-price">от 1 760 руб.</div>
+					</div>
+					<a href="#" class="card-popular__btn">Подробнее...</a>
+				</div>
+			</div>
+
+			<div class="popular__column">
+				<div class="popular__card card-popular">
+					<div class="card-popular__img _ibg">
+						<img src="img/popular/01.jpg" alt="Картинка товара">
+					</div>
+					<div class="card-popular__descp">
+						<h3 class="card-popular__descp-title">Maggiore</h3>
+						<p class="card-popular__descp-subtitle">
+							Суперстойкая глубоко — матовая
+							краска для создания идеальных
+							поверхностей
+						</p>
+						<div class="card-popular__descp-price">от 1 760 руб.</div>
+					</div>
+					<a href="#" class="card-popular__btn">Подробнее...</a>
+				</div>
+			</div>
+
+			<div class="popular__column">
+				<div class="popular__card card-popular">
+					<div class="card-popular__img _ibg">
+					<img src="img/popular/01.jpg" alt="Картинка товара">
+					</div>
+					<div class="card-popular__descp">
+						<h3 class="card-popular__descp-title">Rotonda</h3>
+						<p class="card-popular__descp-subtitle">
+							Суперстойкая глубоко — матовая
+							краска для создания идеальных
+							поверхностей
+						</p>
+						<div class="card-popular__descp-price">от 1 760 руб.</div>
+					</div>
+					<a href="#" class="card-popular__btn">Подробнее...</a>
+				</div>
+			</div>
+
+			<div class="popular__column">
+				<div class="popular__card card-popular">
+					<div class="card-popular__img _ibg">
+					<img src="img/popular/02.jpg" alt="Картинка товара">
+					</div>
+					<div class="card-popular__descp">
+						<h3 class="card-popular__descp-title">Maggiore</h3>
+						<p class="card-popular__descp-subtitle">
+							Суперстойкая глубоко — матовая
+							краска для создания идеальных
+							поверхностей
+						</p>
+						<div class="card-popular__descp-price">от 1 760 руб.</div>
+					</div>
+					<a href="#" class="card-popular__btn">Подробнее...</a>
+				</div>
+			</div>
+
+			<div class="popular__column">
+				<div class="popular__card card-popular">
+					<div class="card-popular__img _ibg">
+					<img src="img/popular/01.jpg" alt="Картинка товара">
+					</div>
+					<div class="card-popular__descp">
+						<h3 class="card-popular__descp-title">Rotonda</h3>
+						<p class="card-popular__descp-subtitle">
+							Суперстойкая глубоко — матовая
+							краска для создания идеальных
+							поверхностей
+						</p>
+						<div class="card-popular__descp-price">от 1 760 руб.</div>
+					</div>
+					<a href="#" class="card-popular__btn">Подробнее...</a>
+				</div>
+			</div>
+
+			<div class="popular__column">
+				<div class="popular__card card-popular">
+					<div class="card-popular__img _ibg">
+					<img src="img/popular/01.jpg" alt="Картинка товара">
+					</div>
+					<div class="card-popular__descp">
+						<h3 class="card-popular__descp-title">Rotonda</h3>
+						<p class="card-popular__descp-subtitle">
+							Суперстойкая глубоко — матовая
+							краска для создания идеальных
+							поверхностей
+						</p>
+						<div class="card-popular__descp-price">от 1 760 руб.</div>
+					</div>
+					<a href="#" class="card-popular__btn">Подробнее...</a>
+				</div>
+			</div>
+
+	</div>
+
+	</div>
+
+</div>
+
+	</div>
+</section>
+
+@endsection 
