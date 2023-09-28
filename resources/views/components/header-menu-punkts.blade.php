d1e6670ec34c1dc3e9ea543496298d19cf13e9c7
asmi046 <asmi046@gmail.com>
2023-09-28 15:26:59 +0300

    Выноска меню

---
+++ resources/views/components/header-menu-punkts.blade.php
@@ -0,0 +1,6 @@
+<li class="menu-list__item"><a href="#" class="menu-list__link">О нас</a></li>
+            <li class="menu-list__item"><a href="#" class="menu-list__link">Магазин</a></li>
+            <li class="menu-list__item"><a href="#" class="menu-list__link">Фактуры</a></li>
+            <li class="menu-list__item"><a href="#" class="menu-list__link">Услуги</a></li>
+            <li class="menu-list__item"><a href="#" class="menu-list__link">Проекты</a></li>
+            <li class="menu-list__item"><a href="#" class="menu-list__link">Контакты</a></li>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,17 +1,6 @@
 <header id="header" class="header">
 
-	<div class="header__top">
-		<div class="header__top-container _container">
-			<ul class="menu-list header__menu-list d-flex">
-				<li class="menu-list__item"><a href="#" class="menu-list__link">О нас</a></li>
-				<li class="menu-list__item"><a href="#" class="menu-list__link">Магазин</a></li>
-				<li class="menu-list__item"><a href="#" class="menu-list__link">Фактуры</a></li>
-				<li class="menu-list__item"><a href="#" class="menu-list__link">Услуги</a></li>
-				<li class="menu-list__item"><a href="#" class="menu-list__link">Проекты</a></li>
-				<li class="menu-list__item"><a href="#" class="menu-list__link">Контакты</a></li>
-			</ul>
-		</div>
-	</div>
+    <x-header-menu></x-header-menu>
 
 	<div class="header__bottom">
 		<div class="header__bottom-container _container">
