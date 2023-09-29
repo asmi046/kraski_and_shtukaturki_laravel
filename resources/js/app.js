8c0bd564ec4ef241189199ac8f72405f52634a85
asmi046 <asmi046@gmail.com>
2023-09-29 20:09:46 +0300

    vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,8 @@ import VueAxios from 'vue-axios'
 import { Swiper, SwiperSlide } from 'swiper/vue';
 import { Pagination } from 'swiper/modules';
 
+import { store } from "./storage"
+
 import 'swiper/css';
 import 'swiper/css/pagination';
 
@@ -25,4 +27,5 @@ const global_app = createApp({
 })
 
 global_app.use(VueAxios, axios)
+global_app.use(store)
 global_app.mount("#global_app")
