13a248cdf02206d061bc9708797d5dfce5ede76e
Ruvick <rudikov.web@ya.ru>
2023-10-01 10:40:15 +0300

    Адаптив блока Фактуры

--- resources/views/components/tovar-page/texture.blade.php
+++ resources/views/components/tovar-page/texture.blade.php
@@ -1,3 +1,4 @@
+<div class="texture-column">
 <div class="texture_blk">
     <div class="img">
         <img src="{{$textura->img}}" alt="Фактура {{$textura->title}}">
@@ -5,4 +6,5 @@
     <div class="title">
         <h3>{{$textura->title}}</h3>
     </div>
+</div>
 </div>
