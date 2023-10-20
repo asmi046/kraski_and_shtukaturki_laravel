a3ae42c725c98ae40101a9d5ab4723ac7458ef35
asmi046 <asmi046@gmail.com>
2023-10-20 12:36:26 +0300

    +

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -25,7 +25,7 @@
 
         <div class="category-sec__wrap">
 
-            <x-filter.sidebar-filter></x-filter.sidebar-filter>
+            <x-filter.sidebar-filter name="Каталог"></x-filter.sidebar-filter>
 
             <div class="category-sec__main main-category">
                 <div class="main-category__inner">
