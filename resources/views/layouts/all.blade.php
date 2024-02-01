6a474de8d6c2192c954637f5919e7a1a7e9c2979
asmi046 <asmi046@gmail.com>
2024-02-01 16:09:21 +0300

    YWebmaster

--- resources/views/layouts/all.blade.php
+++ resources/views/layouts/all.blade.php
@@ -26,6 +26,8 @@
 
     <meta name="_token" content="{{ csrf_token() }}">
 
+    <meta name="yandex-verification" content="4ecab90eb91f7dab" />
+
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <script src="//api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
